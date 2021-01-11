312821eeea606e1d70e0ffff1ed046744c7e9de6
Kuxynator <kuxynator@ksware.de>
2021-01-11 09:51:15 +0100

    v2.1.5

--- src/autotargeter.lua
+++ src/autotargeter.lua
@@ -20,6 +20,7 @@ end
 -- chunk_position :: ChunkPosition: The chunk scanned.
 -- area :: BoundingBox: Area of the scanned chunk.
 script.on_event(defines.events.on_sector_scanned, function(event)
+	--print("on_sector_scanned",serpent.line(event.chunk_position), serpent.line(event.area))
 	local surfaceName = event.radar.surface.name -- TODO same as target.surface?
 	if #global.forces_ion_cannon_table["Queue"] > 0 then
 		local target = global.forces_ion_cannon_table["Queue"][1]
@@ -46,6 +47,12 @@ script.on_event(defines.events.on_sector_scanned, function(event)
 			table.remove(global.forces_ion_cannon_table["Queue"], 1)
 		end
 	end
+
+	--local p = false; if global.permissions[-2] then p = true end
+	--local t = event.radar.force.technologies["auto-targeting"].researched
+	--local s = settings.global["ion-cannon-min-cannons-ready"].value
+	--local c = countIonCannonsReady(event.radar.force, event.radar.surface)
+	--print(p,t,s,c)
 	if global.permissions[-2] then
 		local radar = event.radar
 		local force = radar.force
@@ -65,12 +72,16 @@ script.on_event(defines.events.on_sector_scanned, function(event)
 	end
 end)
 
-script.on_event(defines.events.on_biter_base_built, function(event)
-	if settings.global["ion-cannon-auto-target-visible"].value then
-		local biterBase = event.entity
+--- Called when a biter migration builds a base.
+-- entity :: LuaEntity: The built entity.
+-- Note: This will be called multiple times as each biter in a given migration is sacrificed and builds part of the base.
+script.on_event(defines.events.on_biter_base_built, function(e)
+	--print("on_biter_base_built", serpent.line(e.entity.position))
+	if settings.global["ion-cannon-auto-target-visible"].value then -- Auto-Target New Nests in Visible Regions
+		local biterBase = e.entity
 		for _, force in pairs(game.forces) do
 			if force.technologies["auto-targeting"].researched == true and force.is_chunk_visible(biterBase.surface, Chunk.from_position(biterBase.position)) then
-				--print("insert biter base to Queue")
+				print("insert biter base to Queue")
 				table.insert(global.forces_ion_cannon_table["Queue"], biterBase)
 				return
 			end
--- src/control.lua
+++ src/control.lua
@@ -243,10 +243,12 @@ function update_GUI(player)
 end
 
 function countIonCannonsReady(force, surface) -- TODO check all callers
+	local surfaceName = nil
+	if type(surface) == "string" then surfaceName = surface else surfaceName = surface.name end
 	local ionCannonsReady = 0
 	if global.forces_ion_cannon_table[force.name] then
 		for i, cooldown in pairs(global.forces_ion_cannon_table[force.name]) do
-			if cooldown[2] == 1 and cooldown[3] == surface then
+			if cooldown[2] == 1 and cooldown[3] == surfaceName then
 				ionCannonsReady = ionCannonsReady + 1
 			end
 		end
@@ -255,9 +257,11 @@ function countIonCannonsReady(force, surface) -- TODO check all callers
 end
 
 function timeUntilNextReady(force, surface) -- TODO check all callers
+	local surfaceName = nil
+	if type(surface) == "string" then surfaceName = surface else surfaceName = surface.name end
 	local shortestCooldown = settings.global["ion-cannon-cooldown-seconds"].value
 	for i, cooldown in pairs(global.forces_ion_cannon_table[force.name]) do
-		if cooldown[1] < shortestCooldown and cooldown[2] == 0 and cooldown[3] == surface.name then
+		if cooldown[1] < shortestCooldown and cooldown[2] == 0 and cooldown[3] == surfaceName then
 			shortestCooldown = cooldown[1]
 		end
 	end
